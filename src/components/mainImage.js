e431d6eb9c176981ecd3170ebfa8e8f0cf80ae92
John Spantouris <john.spantouris@gmail.com>
2024-09-30 11:46:42 +0300

    Alt text

--- src/components/mainImage.js
+++ src/components/mainImage.js
@@ -3,7 +3,7 @@ import "./mainImage.css"
 function MainImage() {
   return (
     <div className="mainImageContainer">
-        <img className="mainImage" src="https://picsum.photos/1920/800"/>
+        <img className="mainImage" alt="Wedding announcement" src="https://picsum.photos/1920/800"/>
         <div className="mainText">
             <h1>Σας προσκαλούμε στον γάμο μας και την βάφτιση της κόρης μας</h1>
         </div>
