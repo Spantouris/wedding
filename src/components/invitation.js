694dbfff12cf97c40c1a2d05bb76babd5c7ab842
John Spantouris <john.spantouris@gmail.com>
2024-11-10 11:14:37 +0200

    Invitation text responsive

--- src/components/invitation.js
+++ src/components/invitation.js
@@ -5,10 +5,10 @@ function Invitation() {
         <div className="invitationContainer">
             <div className="invitationCard">
                 <div className="invitationText">
-                    <p className="names spaceBetween">Γιάννης & Μαρία & Νεφέλη</p>
-                    <p className="location spaceBetween">Σας περιμένουμε στο γάμο και την βάφτιση της κόρης μας στο <a href="https://maps.app.goo.gl/fYjP1vEpcKQEcsi87" target="_blank" rel="noreferrer">Ιπποστάσιο Μειμαρίδη</a> στο Κορωπί</p>
+                    <p className="names">Γιάννης & Μαρία & Νεφέλη</p>
+                    <p className="location">Σας περιμένουμε στο γάμο και την βάφτιση της κόρης μας στο <a href="https://maps.app.goo.gl/fYjP1vEpcKQEcsi87" target="_blank" rel="noreferrer">Ιπποστάσιο Μειμαρίδη</a> στο Κορωπί</p>
                     <p>Κυριακή 15 Ιουνίου</p>
-                    <p className="time spaceBetween">18:30</p>
+                    <p className="time">18:30</p>
                     <p>Κουμπάροι</p>
                     <p className="manOfHonor">Μάκης Μήλιος - Ζωή Αγγεπούλου</p>
                 </div>
