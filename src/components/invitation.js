ec4e84da9c4536f180d837599053c6f778b9e73a
John Spantouris <john.spantouris@gmail.com>
2024-11-23 21:18:10 +0200

    Nefeli and css

--- src/components/invitation.js
+++ src/components/invitation.js
@@ -5,8 +5,9 @@ function Invitation() {
         <div className="invitationContainer">
             <div className="invitationCard">
                 <div className="invitationText">
-                    <p className="names">Γιάννης & Μαρία & Νεφέλη</p>
-                    <p className="location">Σας περιμένουμε στο γάμο και την βάφτιση της κόρης μας στο <a href="https://maps.app.goo.gl/fYjP1vEpcKQEcsi87" target="_blank" rel="noreferrer">Ιπποστάσιο Μειμαρίδη</a> στο Κορωπί</p>
+                    <p className="names">Γιάννης & Μαρία</p>
+                    <p>και η μικρή μας Νεφέλη</p>
+                    <p className="location">Σας περιμένουμε στο γάμο μας και την βάφτιση της κόρης μας στο <a href="https://maps.app.goo.gl/fYjP1vEpcKQEcsi87" target="_blank" rel="noreferrer">Ιπποστάσιο Μειμαρίδη</a> στο Κορωπί</p>
                     <p>Κυριακή 15 Ιουνίου</p>
                     <p className="time">18:30</p>
                     <p>Κουμπάροι</p>
